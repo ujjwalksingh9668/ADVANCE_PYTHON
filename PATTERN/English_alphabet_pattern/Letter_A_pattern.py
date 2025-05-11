8fa1e59ec96815ebc04f9fa1788140704c9eb6ec
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-05-11 21:40:02 +0530

    Create Letter_A_pattern.py

---
+++ PATTERN/English_alphabet_pattern/Letter_A_pattern.py
@@ -0,0 +1,17 @@
+# python code to print pattern of capital english alphabet "A"...
+
+# Solution:-
+n = int(input("Enter the height of the letter A: "))
+
+for row in range(n):
+    for col in range(n):
+        # Condition to print '*':
+        if (
+            (col == 0 or col == n - 1) and row != 0 or
+            (row == n // 2 and (col > 0 and col < n - 1)) or
+            (row == 0 and (col > 0 and col < n - 1))
+        ):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
