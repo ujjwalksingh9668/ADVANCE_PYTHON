b332b718d0b4ba7b7494cd7d4599a305f170faf8
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-05-17 00:03:16 -0700

    Create Count_Squares.py

---
+++ GFG/Count_Squares.py
@@ -0,0 +1,11 @@
+"""
+Given a positive integer n, find the number of perfect squares that are less than n in the
+sample space of perfect squares.
+"""
+#https://www.geeksforgeeks.org/problems/count-squares3649/1?page=1&category=Mathematical&sortBy=submissions
+
+#Solution:-
+import math
+n = int(input())
+a = int(math.sqrt(n-1))
+print(a)
