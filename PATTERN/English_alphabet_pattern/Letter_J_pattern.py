6e4eab40fdbbde2c1ba4006d2d19bd71c53c0472
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-05-27 07:11:49 -0700

    Create Letter_J_pattern.py

---
+++ PATTERN/English_alphabet_pattern/Letter_J_pattern.py
@@ -0,0 +1,17 @@
+# python code to print the English alphabet "C"...
+
+# Solution:-
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if row == 0:
+            print("*",end=" ")
+        elif col == n // 2:
+            print("*",end=" ")
+        elif row == n - 1 and (col <= n // 2):
+            print("*",end=" ")
+        elif col == 0 and (row >= n // 2):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
