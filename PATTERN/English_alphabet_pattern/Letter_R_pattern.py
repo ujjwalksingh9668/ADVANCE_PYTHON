0ca72b69952fc64b7e82401918b83418a96f1b5d
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-06-15 09:47:39 -0700

    Create Letter_R_pattern.py

---
+++ PATTERN/English_alphabet_pattern/Letter_R_pattern.py
@@ -0,0 +1,17 @@
+# python code to print the English alphabet "R"...
+
+# Solution:-
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if col == 0:
+            print("*",end=" ")
+        elif row == 0 or row == n // 2:
+            print("*",end=" ")
+        elif col == n - 1 and row != 0 and row != n // 2 and row <= n // 2:
+            print("*",end=" ")
+        elif row - col == n // 2  - 1 or col == n - 1 and row <= n // 2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
