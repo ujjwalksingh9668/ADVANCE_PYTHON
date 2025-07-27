49475b38b9b78abc46b41758b899f209b9e89d99
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-07-27 06:40:52 -0700

    Create Check_pallindrome.py

---
+++ GFG/STRING_AND_LIST/Check_pallindrome.py
@@ -0,0 +1,25 @@
+"""
+Given a string s, you need to check if it is palindrome or not. A palidrome is a string
+that reads the same from front and back.
+
+Examples:
+Input: s = "Hello"
+Output: false
+Explanation: Hello is not equal to olleH so it's not a palindrome.
+
+https://www.geeksforgeeks.org/batch/da-skill-up/track/su-da-day4/problem/check-palindrome--141628
+"""
+
+#Solution:-
+def isPallindrome(s):
+    s = s.replace(" ","").lower()
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+#Drivers code:-
+s = input("enter the string:")
+if isPallindrome(s):
+    print("True")
+else:
+    print("False")
