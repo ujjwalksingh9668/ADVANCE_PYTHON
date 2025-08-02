8e993817273874452867cc898f222b1531d4144b
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-08-02 11:09:36 -0700

    Update Check_pallindrome.py

--- GFG/Check_pallindrome.py
+++ GFG/Check_pallindrome.py
@@ -1,6 +1,6 @@
 """
 Given a string s, you need to check if it is palindrome or not. A palidrome is a string
-that reads the same from front and back.
+that reads the same from front and back....
 
 Examples:
 Input: s = "Hello"
